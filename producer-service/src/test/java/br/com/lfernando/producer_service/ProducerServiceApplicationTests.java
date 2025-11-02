78cc06cccc7a69ed9fa69f704cfb8f6329ddac89
Luiz Fernando <Luiz Fernando>
2025-11-02 18:56:15 -0300

    project created

---
+++ producer-service/src/test/java/br/com/lfernando/producer_service/ProducerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lfernando.producer_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
