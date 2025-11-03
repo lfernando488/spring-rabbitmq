d73b5dccd24bcf8b8baf03627dca4b978df78e3b
Luiz Fernando <Luiz Fernando>
2025-11-03 01:06:05 -0300

    Consumer service

---
+++ consumer-service/src/main/java/br/com/lfernando/consumer_service/rabbitmq/consumers/MessageConsumer.java
@@ -0,0 +1,20 @@
+package br.com.lfernando.consumer_service.rabbitmq.consumers;
+
+import lombok.extern.log4j.Log4j2;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Log4j2
+@Component
+public class MessageConsumer {
+
+    private static final Logger log = LoggerFactory.getLogger(MessageConsumer.class);
+
+    @RabbitListener(queues = {"product.log"})
+    public void consumerLog(String message) {
+       log.info("Consumer received a message: "+ message );
+    }
+
+}
---
+++ consumer-service/src/test/java/br/com/lfernando/consumer_service/ConsumerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lfernando.consumer_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
